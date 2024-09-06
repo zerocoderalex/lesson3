9d07fd26b0583b9fbfed3e33b74c7e1ce8b42dd3
zerocoderalex <alex12449@mail.ru>
2024-09-06 23:20:28 +0500

    add const

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 pygame.display.set_caption('Игра Тир')
+icon = pygame.image.load('')
 
 running = True
 while running:
