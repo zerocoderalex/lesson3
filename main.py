e9135f86f5d9754803e7860d40d5a4a2d46c0043
zerocoderalex <alex12449@mail.ru>
2024-09-06 20:16:46 +0500

    caption

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ pygame.init()
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+pygame.display.set_caption('Игра Тир')
+
 running = True
 while running:
  pass
